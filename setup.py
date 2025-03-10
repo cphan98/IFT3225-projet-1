f92aa9e5916bc8605cdde307980e51e9ce1bf9d9
Laura Cadillo <lauracadillo13@gmail.com>
2025-03-09 23:04:10 -0400

    changed setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name="tp1",
     version="0.1",
-    py_modules=["HoangThiThiCynthia.Phan_Laura.CadilloManrique_extract", "HoangThiThiCynthia.Phan_Laura.CadilloManrique_genere"],
+    py_modules=["extract", "genere"],
     entry_points={
         "console_scripts": [
-            "extract=HoangThiThiCynthia.Phan_Laura.CadilloManrique_extract:main",
-            "genere=HoangThiThiCynthia.Phan_Laura.CadilloManrique_genere:main",
+            "extract=extract:main",
+            "genere=genere:main",
         ],
     },
 )
